1a68f58d5f753cfcbfb7f7e0a7225c421cf9f3ab
Patrick Maher <patrickemaher@yahoo.com>
2021-05-13 08:21:57 -0700

    Adding react router

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,21 @@
 import React from 'react';
+import { HashRouter, Route, Switch } from 'react-router-dom';
 
 import Application from './components/App';
 import './App.css';
 
 function App() {
   return (
-    <Application children={<div>test</div>}></Application>
+    <HashRouter>
+        <Switch>
+            <Route exact path="/">
+                <Application children={<div>test</div>}></Application>
+            </Route>
+            <Route exact path="/address">
+                <Application children={<div>address</div>}></Application>
+            </Route>
+        </Switch>
+    </HashRouter>
   );
 }
 
